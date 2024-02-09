34aea46377fa340e7a0774e128246634075e37fc
Sławek Zieliński <121642873+SlawekZet@users.noreply.github.com>
2024-02-09 22:56:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { getUrl, getOrgUrl, shortenUrl } from "./src/urls.js";
+import { connectToDatabase } from "./src/dbconnect.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+const PORT = 3333;
+
+app.use(express.json());
+
+connectToDatabase();
+
+//get a record from the db based on the originalUrl passed in the body
+app.get("/get-url", getUrl);
+
+//get an original url based on the short url path passed in the body
+app.get("/get-org-url", getOrgUrl);
+
+//shorten a provided url
+app.post("/shorten", shortenUrl);
+
+app.listen(PORT, () => {
+  console.log(`Server is running o the port ${PORT}`);
+});
